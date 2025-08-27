47f762209693d6bfef48ef3b7fb5563890fbe778
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2025-08-27 10:36:31 -0300

    feat: listar usuario

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -10,6 +10,7 @@ router.get('/imoveis', authenticateLogin, ImoveisController.ListImoveis)
 router.get('/imoveis/favoritos', authenticateLogin ,ImoveisController.ListFavorites)
 
 //usuario
+router.get('/usuario', authenticateLogin, UserController.getUser)
 router.post('/usuario', authenticateLogin, adminVerification, UserController.postUser)
 router.post('/usuario/login', UserController.postLogin)
 router.patch('/usuario', UserController.patchUser)
